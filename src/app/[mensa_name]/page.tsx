09c8c810a31bd5356b49499221adc7c4d020825b
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-05-04 08:59:43 +0200

    feat: only show upcoming three days

--- src/app/[mensa_name]/page.tsx
+++ src/app/[mensa_name]/page.tsx
@@ -2,12 +2,16 @@ import { scrape } from "@/core/scrape"
 
 export default async function Mensa({ params }: { params: { mensa_name: string } }) {
 	const days = await scrape(params.mensa_name)
+	const today = new Date()
+	today.setHours(0, 0, 0, 0)
+
+	const upcoming = days.filter(day => day.date >= today).slice(0, 3)
 
 	return (
 		<div className="min-h-screen">
 			<p>{params.mensa_name}</p>
 			<ul>
-				{days.map((day) => (
+				{upcoming.map((day) => (
 					<li key={day.date.getTime()}>{day.date.toLocaleDateString("de-DE")}: <ul>{day.meals.map(meal => <li key={meal.name}>[{meal.category}] {meal.name} +{meal.additions.length} ({meal.price}) - Allergens: {JSON.stringify(meal.allergens)}</li>)}</ul></li>
 				))}
 			</ul>
