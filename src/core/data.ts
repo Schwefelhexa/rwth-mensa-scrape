82e14271b19c8f72ecad08106d5408faacec1d2f
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-05-06 17:51:29 +0200

    feat: list all known mensas

--- src/core/data.ts
+++ src/core/data.ts
@@ -1,11 +1,12 @@
 export interface Mensa {
 	display_name: string;
 	slug: string;
+	address: string;
 }
 
 export const mensas: Mensa[] = [
-	{ display_name: 'Academica', slug: 'academica' },
-	{ display_name: 'Vita', slug: 'vita' },
-	{ display_name: 'Ahornstraße', slug: 'ahornstrasse' },
+	{ display_name: 'Academica', slug: 'academica', address: 'Pontwall 3, 52062 Aachen' },
+	{ display_name: 'Vita', slug: 'vita', address: 'Helmertweg 1, 52074 Aachen' },
+	{ display_name: 'Ahornstraße', slug: 'ahornstrasse', address: 'Ahornstraße 55, 52074 Aachen' },
 ]
 export const getMensa = (slug: string) => mensas.find(m => m.slug === slug);
